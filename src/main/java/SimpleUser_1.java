652447252cc2294e0d3c68f745cfe585f22e2304
shakibamirbagheri <shakibamirbagheri@Host-068.homenet.telecomitalia.it>
2025-07-25 12:04:39 +0200

    Add generated code for task: SimpleUser

---
+++ src/main/java/SimpleUser_1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class UUID {
+    private static final String uuid = "1234567890"; // Private field to store a unique identifier
+    public UUID() {}
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+    public String getUuid() {
+        return uuid;
+    }
+}
